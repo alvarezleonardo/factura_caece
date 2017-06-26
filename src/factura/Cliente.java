0edc6e5ecc28fddf7e3c30f1f4649470104a9775
alvarezleonardo <leonardo.alvarez@live.com.ar>
2017-06-26 11:04:33 -0300

    Agregamos clases localidad, privincia, pais y partido. Actualizamos cliente

--- src/factura/Cliente.java
+++ src/factura/Cliente.java
@@ -10,7 +10,41 @@ package factura;
  * @author alumno
  */
 public class Cliente {
-    private String nombre;
+    private String nombre, apellido, calle;
+    private Integer dni, altura;
+    private Localidad localidad;
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public Localidad getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(Localidad localidad) {
+        this.localidad = localidad;
+    }
 
     public String getNombre() {
         return nombre;
